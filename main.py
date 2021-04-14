3430d844b49c4ec3137d158b93785f4a17cf0d87
Blake-webm <theblake2004@gmail.com>
2021-04-14 16:44:20 +0400

    Трошин
    Небольшие доработки, но все также db не создается

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+
 # базовый дизайн был взят из Boodstrap
 obb = Flask(__name__)
 # Создаем настройку Sql
-obb.config['SQLALCHEMY DATABASE_URI'] = "sqlite:///BigShop.db"
+obb.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///BigShop.db"
+obb.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(obb)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,14 +15,25 @@ class Item(db.Model):
     price = db.Column(db.Integer, nullable=False)
     isActive = db.Column(db.Boolean, default=True)
 
+
 # делаем возможным отслеживание странички
 @obb.route("/")
 def home_index():
     return render_template("main_title.html")
+
+
 # страничка о нас
 @obb.route("/about")
-def aboun_us():
+def about_us():
     return render_template("aboutOfUs.html")
+
+
+# страничка создания
+@obb.route("/create", methods=['POST', "GET"])
+def create():
+    return render_template("create.html")
 # проверяем какой файл запускаеться
+
+
 if __name__ == "__main__":
     obb.run(debug=True)
